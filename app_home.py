ebc3c54c5bcec4cb4925500c11b88e726aed7658
deyeon <deyeon209@gmail.com>
2022-12-13 16:49:20 +0900

    데이터 분석

---
+++ app_home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def run_home_app():
+    st.text('이 앱은 고객데이터와 자동차 구매 데이터에 대한 내용입니다. 데이터 분석 및 고객 정보를 넣으면,\얼마정도의 구매금액을 예측해 줍니다.')
+
+    st.image('https://m.young.hyundai.com:444/upload/CMS_NEWS_IMAGE/2016/03/23/CMS_NEWS_IMAGE_PIWCZj9NqnJSzHPc4AUs.png')
