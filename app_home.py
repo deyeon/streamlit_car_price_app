1b3ecc6bc1543e2237c5b00e27ce7d116c1956a7
deyeon <deyeon209@gmail.com>
2022-12-20 11:49:43 +0900

    Update app_home.py

--- app_home.py
+++ app_home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 def run_home_app():
+    st.subheader('welcome~')
     st.text('이 앱은 고객데이터와 자동차 구매 데이터에 대한 내용입니다. 데이터 분석 및 고객 정보를 넣으면,\얼마정도의 구매금액을 예측해 줍니다.')
     st.text('안녕하세요')
 
